c4c36cdfcb76cc2b83e2391f49d89712d4af8409
Jorge Gaitan <info@dinnovos.com>
2016-01-27 12:58:02 -0430

    Cambio de los parametros de la clase Symfony\Component\HttpKernel\EventListener\RouterListener, cuyo segundo componente ahora debe ser Symfony\Component\HttpFoundation\RequestStack y no null.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Kodazzi Framework.
  *
- * (c) Jorge Gaitan <jgaitan@kodazzi.com>
+ * (c) Jorge Gaitan <info@kodazzi.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -63,7 +63,7 @@ Service::set('kernel.url_generator', function(){
 });
 
 Service::set('listener.router', function(){
-    return new Symfony\Component\HttpKernel\EventListener\RouterListener(Service::get('kernel.matcher'), null, null, Service::get('kernel.stack'));
+    return new Symfony\Component\HttpKernel\EventListener\RouterListener(Service::get('kernel.matcher'), Service::get('kernel.stack'), null, null);
 });
 
 Service::set('listener.controller', function(){
